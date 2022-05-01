589dbb8fe42658e2b1ad7c64b9b6d8b7b187bb8b
abdulrahmanabdullah <nfs056@gmail.com>
2022-05-02 00:57:00 +0300

    update db path in env file and setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-export DATABASE_URL="postgres://zhvkjfxcxdikcd:c134fdc35e26448b54d196269bee82a05fcf963d01c97d9c31a2eb6bad663fbf@ec2-44-194-4-127.compute-1.amazonaws.com:5432/dmhh9jm24bqni
+export DATABASE_URL="postgresql://zhvkjfxcxdikcd:c134fdc35e26448b54d196269bee82a05fcf963d01c97d9c31a2eb6bad663fbf@ec2-44-194-4-127.compute-1.amazonaws.com:5432/dmhh9jm24bqni
 "
