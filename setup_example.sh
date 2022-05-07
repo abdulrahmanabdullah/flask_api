bd558e35b254898b27ac3266b0fc803cce0a4a27
abdulrahmanabdullah <nfs056@gmail.com>
2022-05-07 21:36:46 +0300

    remove senstive files

---
+++ setup_example.sh
@@ -0,0 +1,5 @@
+export DATABASE_URL='postgresql://path_from_heroku'
+export AUTH0_DOMAIN='Auth0_Domain'
+export ALGORITHMS=['RS256']
+export API_AUDIANC='AUTH0_API_AUDIANC'
+export ASSISATN_JWT='access token from auth0'
